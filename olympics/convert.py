a4732e5ff78c50f1766f68fc5fbc6654f02374f4
Jayti Arora <jaytiarora@Jaytis-MacBook-Pro.local>
2021-10-13 23:54:31 -0500

    Creates olympics db

---
+++ olympics/convert.py
@@ -0,0 +1,23 @@
+#Step 1
+#Design a database to represent the data from the Kaggle CSV files. 
+#Write your design in the form of CREATE TABLE statements, and save those statements 
+#(and only the CREATE TABLE statements, including semi-colons) in a text file called olympics-schema.sql.
+
+#Step 2
+#read the Kaggle CSV files and write one CSV file 
+#for each of the of the tables in your database design.
+
+# If you have designed a table named "athletes", for example, 
+# the corresponding CSV file should be named "athletes.csv", and should have the same columns as the table.
+
+#Step 3
+#Add each of your new CSV files to your repository.
+#(You may, if you wish, also add the original Kaggle files to your repository. 
+#But it's not necessary. That athlete_events.csv file is kind of a beast at 30MB, 
+#and you may prefer to just link to the originals at the top of convert.py or something like that.)
+
+#Step 4
+#Write SQL queries in queries.sql
+
+
+
