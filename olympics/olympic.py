936fcbc641c2966ccfd701cba1054e8c4169c869
Jayti Arora <jaytiarora@Jaytis-MacBook-Pro.local>
2021-10-22 00:49:50 -0500

    Added untracked csv files

---
+++ olympics/olympic.py
@@ -0,0 +1,13 @@
+'''
+
+Prints a usage statement for "python3 olympics.py -h" (or --help). 
+You may use argparse or not for command-line parsing and usage statement printing.
+
+List the names of all the athletes from a specified NOC.
+
+List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
+
+One more operation of your choosing. Simple is fine, crazily-ambitious is also fine. 
+(Both are worth the same number of points, though crazily-ambitious may earn you more points on the Cosmic Scoreboard. 
+Or fewer. I have no idea how the cosmos keeps score.)
+'''
